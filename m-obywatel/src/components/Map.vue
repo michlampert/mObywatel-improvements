cb2a1ae35e85fdfbdbf556e9f82affec9e645d51
tmek1244 <tmek1244@wp.pl>
2023-10-21 20:44:45 +0200

    Add map component

--- m-obywatel/src/components/Map.vue
+++ m-obywatel/src/components/Map.vue
@@ -1,10 +1,44 @@
 <template>
-    <ion-img alt="dupa" src="../../resources/mapa_pl.jpg">
-    </ion-img>
+  <div style="height:600px; width:800px">
+    <l-map ref="map" :zoom="zoom" :center="[lat, lon]" :use-global-leaflet="false">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+
+      ></l-tile-layer>
+        <l-marker :lat-lng="[lat, lon]"></l-marker>
+
+        <l-marker v-for="location in locations" :lat-lng="[location.lat, location.lon]">
+            <l-popup>
+                <p>{{ location.name }}</p>
+            </l-popup>
+        </l-marker>
+    </l-map>
+    {{ zoom }}
+  </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "@vue-leaflet/vue-leaflet";
 
+const props = defineProps({
+    zoom: {
+        type: Number,
+        default: '1',
+    },
+    lat: {
+        type: Number,
+    },
+    lon: {
+        type: Number,
+    },
+    locations: {
+        type: Object,
+    },
+});
 </script>
 
-<style scoped></style>
+<style>
+</style>
