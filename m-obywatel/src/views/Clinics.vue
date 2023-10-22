53f26503c3180323b5bd0b912c1da966f805a53e
Michal Lampert <michlampert@gmail.com>
2023-10-22 06:19:04 +0200

    Add Sor and Aed views

--- m-obywatel/src/views/Clinics.vue
+++ m-obywatel/src/views/Clinics.vue
@@ -41,52 +41,34 @@
         <ion-item>
           <SearchButton @click="search()" :required-fields="[address, treatment]"></SearchButton>
         </ion-item>
-        <ion-item>
-          <ViewModeButton v-model="viewMode"></ViewModeButton>
-        </ion-item>
+
       </ion-list>
 
       <div class="center">
         <ion-spinner v-if="loading" name="circular" size="large" color="medium"></ion-spinner>
       </div>
-      <!-- {{ viewMode }} -->
-      <Transition name="list">
-        <template v-if="viewMode == 'list'">
-          <ion-list>
-            <!-- {{ clinics }} -->
-            <ion-item lines="none" v-for="clinic in clinics">
-              <Place :currentLocation="currentLocation" :place="clinic" :places="[clinic]">
-              </Place>
-            </ion-item>
-          </ion-list>
-        </template>
-      </Transition>
-      <Transition name="map">
-        <template v-if="viewMode == 'map'">
-            <Map :current-location="currentLocation" v-bind:other-places="clinics.map(val => val as PlacesCombined)">
-            </Map>
-        </template>
-      </Transition>
+
+
+
+      <PlacesPresentation v-model:current-location="currentLocation" v-model:places="clinics"></PlacesPresentation>
 
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import Place from "@/components/Place.vue"
 import Toolbar from '@/components/Toolbar.vue'
 import { IonPage, IonHeader, IonContent, IonSpinner } from '@ionic/vue';
 import { analytics, optionsOutline, timerOutline } from 'ionicons/icons';
 import { Ref, onMounted, ref } from "vue";
-import { Clinic, PlacesCombined } from "@/api/model"
-import { getClinics, getCurrentLocation, getPossibleTreatments } from "@/api/api"
+import { Clinic } from "@/api/model"
+import { getClinics, getPossibleTreatments } from "@/api/api"
 import Alert from "@/components/Alert.vue"
 import City from "@/components/City.vue"
 import SearchButton from "@/components/SearchButton.vue";
 
-import { IonList, IonItem, IonSegment, IonSegmentButton, IonIcon, IonButton, IonText, IonInput, IonCard } from "@ionic/vue";
-import ViewModeButton from "@/components/ViewModeButton.vue";
-import Map from "@/components/Map.vue";
+import { IonList, IonItem, IonSegment, IonSegmentButton, IonIcon, IonInput } from "@ionic/vue";
+import PlacesPresentation from "@/components/PlacesPresentation.vue";
 
 const clinics: Ref<Clinic[]> = ref([])
 const address: Ref<string> = ref("")
@@ -94,7 +76,6 @@ const loading: Ref<boolean> = ref(false)
 // const treatment: Ref<string> = ref("")
 const treatment: Ref<string> = ref("ODDZIAŁ CHIRURGICZNY OGÓLNY")
 const treatments: Ref<{ name: string, id: string }[]> = ref([])
-const viewMode: Ref<"list" | "map"> = ref("list")
 const searchMode: Ref<number> = ref(30)
 
 const currentLocation = {
@@ -135,28 +116,5 @@ ion-spinner {
   flex-direction: row;
   justify-content: center;
   align-items: center;
-}
-
-.suggestions {}
-
-.list-enter-active,
-.list-leave-active {
-  transition: transform 0.5s ease;
-}
-
-.list-enter-from,
-.list-leave-to {
-  transform: translateX(-100%);
-}
-
-
-.map-enter-active,
-.map-leave-active {
-  transition: transform 0.5s ease;
-}
-
-.map-enter-from,
-.map-leave-to {
-  transform: translateX(100%);
 }
 </style>
--- m-obywatel/src/views/SORs.vue
+++ m-obywatel/src/views/SORs.vue
@@ -8,31 +8,57 @@
       </Toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <Map :zoom="zoom" :current-location=" { latitude: 50.2, longitude: 19 }" :other-locations="locations"/>
+      <ion-list lines="none" class="padding-top">
+        <ion-item>
+          <City v-model:address="address"></City>
+        </ion-item>
+        <ion-item>
+          <SearchButton @click="search()" :required-fields="[address]"></SearchButton>
+        </ion-item>
+
+      </ion-list>
+
+      <div class="center">
+        <ion-spinner v-if="loading" name="circular" size="large" color="medium"></ion-spinner>
+      </div>
+
+      <PlacesPresentation v-model:current-location="currentLocation" v-model:places="places"></PlacesPresentation>
+
     </ion-content>
   </ion-page>
-
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonContent } from '@ionic/vue';
+import Toolbar from '@/components/Toolbar.vue'
+import { IonPage, IonHeader, IonContent, IonSpinner } from '@ionic/vue';
+import { Ref, ref } from "vue";
+import { Place } from "@/api/model"
+import {  getSORs } from "@/api/api"
+import City from "@/components/City.vue"
+import SearchButton from "@/components/SearchButton.vue";
 
-import Map from '@/components/Map.vue'
+import { IonList, IonItem, IonSegment, IonSegmentButton, IonIcon, IonInput } from "@ionic/vue";
+import PlacesPresentation from "@/components/PlacesPresentation.vue";
 
-import Toolbar from '@/components/Toolbar.vue';
+const places: Ref<Place[]> = ref([])
+const address: Ref<string> = ref("")
+const loading: Ref<boolean> = ref(false)
+const searchMode: Ref<number> = ref(30)
 
-let zoom = 7;
+const currentLocation = {
+  latitude: 54.21093325,
+  longitude: 22.000529771276238,
+}
 
-const locations = [{
-    latitude: 51.21093325,
-    longitude: 18.000529771276238,
-    name: 'test',
-  }, {
-    latitude: 52.21093325,
-    longitude: 17.000529771276238,
-    name: 'test',
-  }];
-console.log(locations)
+function search() {
+  loading.value = true
+  places.value = []
+  getSORs(currentLocation, searchMode.value)
+    .then(value => {
+      loading.value = false
+      places.value = value
+    })
+}
 
 
 </script>
